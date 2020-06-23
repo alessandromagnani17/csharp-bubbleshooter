5e574b4b2ff297ba9d1869306906e10ffc835195
giacomo accursi <giacomo.accursi@gmail.com>
2020-06-23 17:13:33 +0200

    create Point2D class

---
+++ oop19-bubbleshooter-csharp/csharp-tasks/Accursi Giacomo/Point2D.cs
@@ -0,0 +1,14 @@
+namespace csharp_tasks.Accursi_Giacomo
+{
+    public class Point2D
+    {
+        private int X { get;}
+        private int Y { get;}
+
+        public Point2D(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+}
