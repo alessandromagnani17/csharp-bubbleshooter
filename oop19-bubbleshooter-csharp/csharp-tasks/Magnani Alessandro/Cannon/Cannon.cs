c0ef47d92a56aa4a66234ea9043a596014ac82f5
Your Name <alessandro.magnani18@studio.unibo.it>
2020-06-24 21:48:14 +0200

    Modified Cannon.cs

--- oop19-bubbleshooter-csharp/csharp-tasks/Magnani Alessandro/Cannon/Cannon.cs
+++ oop19-bubbleshooter-csharp/csharp-tasks/Magnani Alessandro/Cannon/Cannon.cs
@@ -9,37 +9,37 @@ namespace csharp_tasks.Magnani_Alessandro.Cannon
         private double angle;
 
         /**
-     * Constructor for a new Cannon.
-     * 
-     * @param img , the image of Cannon.
-     */
+         * Constructor for a new Cannon.
+         * 
+         * @param img , the image of Cannon.
+         */
         public Cannon(Image img) 
         {
             this.imageCannon = new ImageView(img);
         }
 
         /**
-     * Method to get the {@link ImageView} of the {@link Cannon}.
-     * @return the {@link ImageView} of the {@link Cannon}.
-     */
+         * Method to get the {@link ImageView} of the {@link Cannon}.
+         * @return the {@link ImageView} of the {@link Cannon}.
+         */
         public readonly ImageView GetCannon() 
         {
             return imageCannon;
         }
 
         /**
-     * Method to get the angle of {@link Cannon} rotation.
-     * @return the angle of {@link Cannon}.
-     */
+         * Method to get the angle of {@link Cannon} rotation.
+         * @return the angle of {@link Cannon}.
+         */
         public readonly double GetAngle() 
         {
             return angle;
         }
 
         /**
-     * Method to set the angle of {@link Cannon} rotation.
-     * @param angle , the angle of {@link Cannon}.
-     */
+         * Method to set the angle of {@link Cannon} rotation.
+         * @param angle , the angle of {@link Cannon}.
+         */
         public readonly void SetAngle(double angle) 
         {
             this.angle = angle;
