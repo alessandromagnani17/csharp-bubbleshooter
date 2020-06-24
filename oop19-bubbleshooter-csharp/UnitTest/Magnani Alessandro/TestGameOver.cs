48e2f088a26b3628597f47d681b44ef5b309f760
Your Name <alessandro.magnani18@studio.unibo.it>
2020-06-24 21:36:59 +0200

    Added TestSwitchingBubble.cs

---
+++ oop19-bubbleshooter-csharp/UnitTest/Magnani Alessandro/TestGameOver.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest.Magnani_Alessandro
+{
+    [TestClass]
+    class TestGameOver
+    {
+        private static readonly double BUBBLE_POSITION_TRUE  = Model.WORLD_HEIGHT / 1.1;
+        private static readonly double BUBBLE_POSITION_FALSE = 0;
+
+        private readonly Level basicMode = new BasicLevel();
+        private readonly Level survivalMode = new SurvivalLevel();
+
+        /**
+         * Method to test if {@link GameOverChecker} launch a GameOver
+         * to the basic mode.
+         */
+        
+        [TestMethod]
+        public void TestBasicGameOver() {
+            GameOverChecker gameOverChecker = new GameOverChecker(this.basicMode);
+            Assert.IsTrue(gameOverChecker.IsGameOver(BUBBLE_POSITION_TRUE));
+            Assert.IsFalse(gameOverChecker.IsGameOver(BUBBLE_POSITION_FALSE));
+        }
+
+        /**
+         * Method to test if {@link GameOverChecker} launch a GameOver
+         * to the survival mode.
+         */
+        
+        [TestMethod]
+        public void TestSurvivalGameOver() {
+            GameOverChecker gameOverChecker = new GameOverChecker(this.survivalMode);
+            Assert.IsTrue(gameOverChecker.IsGameOver(BUBBLE_POSITION_TRUE));
+            Assert.IsFalse(gameOverChecker.IsGameOver(BUBBLE_POSITION_FALSE));
+        }
+    }
+}
---
+++ oop19-bubbleshooter-csharp/UnitTest/Magnani Alessandro/TestSwitchingBubble.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest.Magnani_Alessandro
+{
+    [TestClass]
+    class TestSwitchingBubble
+    {
+        private static readonly double LIMITS = 3;
+
+        private readonly Bubble switchBubble = new SwitchBubble(new Point(0, 0), BubbleColor.GREEN);
+        private readonly Bubble testShootingBubble = new ShootingBubble(new Point(0, 0), BubbleColor.BLUE);
+        private readonly List<Bubble> bubblesManager = new LinkedList<>();
+        private readonly SwitcherController switcherController = new SwitcherController(bubblesManager);
+        private readonly Level basicLevel = new BasicLevel();
+        private readonly Level survivalLevel = new SurvivalLevel();
+
+        
+        /**
+         * Method to test if the {@link ShootingBubble} at the switch takes on the color of the {@link SwitchBubble}.
+         */
+        [TestMethod]
+        public void TestSwitch() {
+            this.bubblesManager.Add(this.testShootingBubble);
+            this.bubblesManager.Add(this.switchBubble);
+            this.switcherController.SetBubbles(bubblesManager);
+            this.switcherController.SwitchControl();
+
+            Console.WriteLine(this.switchBubble.GetColor());
+
+            Assert.IsTrue(this.switchBubble.GetColor().Equals(BubbleColor.BLUE));
+            Assert.IsTrue(this.testShootingBubble.GetColor().Equals(BubbleColor.GREEN));
+        }
+
+        /**
+         * Method to test if the {@link ShootingBubble} doesn't take on the 
+         * {@link SwitchBubble}'s color when the Switch Limit is exceeded.
+         */
+        
+        
+        [TestMethod]
+        public void TestLimitsExceeded() {
+            this.bubblesManager.Add(this.testShootingBubble);
+            this.bubblesManager.Add(this.switchBubble);
+            this.switcherController.SetBubbles(bubblesManager);
+            this.switcherController.SetNumSwitch(LIMITS);
+            this.switcherController.SwitchControl();
+
+            Assert.IsFalse(this.switchBubble.GetColor().Equals(BubbleColor.BLUE));
+            Assert.IsFalse(this.testShootingBubble.GetColor().Equals(BubbleColor.GREEN));
+        }
+
+        /**
+         * Method to test if the {@link ShootingBubble} takes on the color 
+         * of the {@link SwitchBubble} after Shooting in {@link BasicMode}.
+         */
+        
+        [TestMethod]
+        public void TestBasicSwitchAfterShot() {
+            this.bubblesManager.Add(testShootingBubble);
+            this.bubblesManager.Add(switchBubble);
+            this.basicLevel.GetBubblesManager().AddBubbles(bubblesManager);
+            this.basicLevel.LoadShootingBubble();
+
+            Assert.IsTrue(this.basicLevel.GetBubblesManager().GetShootingBubble().Get().GetColor().Equals(BubbleColor.GREEN));
+        }
+
+        /**
+         * Method to test if the {@link ShootingBubble} takes on the color 
+         * of the {@link SwitchBubble} after Shooting in {@link SurvivalMode}.
+         */
+        
+        [TestMethod]
+        public void TestSurvivalSwitchAfterShot() {
+            this.bubblesManager.Add(testShootingBubble);
+            this.bubblesManager.Add(switchBubble);
+            this.survivalLevel.GetBubblesManager().AddBubbles(bubblesManager);
+            this.survivalLevel.LoadShootingBubble();
+
+            Assert.IsTrue(this.survivalLevel.GetBubblesManager().GetShootingBubble().Get().GetColor().Equals(BubbleColor.GREEN));
+        }
+    }
+}
